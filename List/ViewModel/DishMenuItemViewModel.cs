98305c1114f8a2b28cb4a5fb6c7c6bb6cf67f846
ydtg1993 <946818508@qq.com>
2018-12-16 23:48:09 +0800

    first commit

---
+++ List/ViewModel/DishMenuItemViewModel.cs
@@ -0,0 +1,28 @@
+﻿using List.Model;
+using Microsoft.Practices.Prism.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List.ViewModel
+{
+    class DishMenuItemViewModel: NotificationObject
+    {
+        public Dish Dish { get; set; }
+
+        private bool isSelected;
+
+        public bool IsSelected
+        {
+            get { return isSelected; }
+            set
+            {
+                isSelected = value;
+                this.RaisePropertyChanged("IsSelected");
+            }
+        }
+    }
+}
---
+++ List/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,95 @@
+﻿using List.Model;
+using List.Services;
+using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace List.ViewModel
+{
+    class MainWindowViewModel:NotificationObject
+    {
+        public DelegateCommand PlaceOrderCommand { set; get; }
+        public DelegateCommand SelectMenuItemCommand { set; get; }
+
+        private int count;
+
+        public int Count
+        {
+            get { return count; }
+            set
+            {
+                count = value;
+                this.RaisePropertyChanged("Count");
+            }
+        }
+
+        private Restaurant restaurant;
+
+        public Restaurant Restaurant
+        {
+            get { return restaurant; }
+            set
+            {
+                restaurant = value;
+                this.RaisePropertyChanged("Restaurant");
+            }
+        }
+
+        private List<DishMenuItemViewModel> dishMenu;
+
+        public List<DishMenuItemViewModel> DishMenu
+        {
+            get { return dishMenu; }
+            set
+            {
+                dishMenu = value;
+                this.RaisePropertyChanged("DIshMenu");
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+            this.LoadRestaurant();
+            this.LoadDishMenu();
+            this.PlaceOrderCommand = new DelegateCommand(new Action(this.PlaceOrderCommandExecute));
+            this.SelectMenuItemCommand = new DelegateCommand(new Action(this.SelectMenuItemExecute));
+        }
+
+        private void LoadDishMenu()
+        {
+            this.Restaurant = new Restaurant();
+            this.Restaurant.Name = "hikki";
+            this.Restaurant.Address = "四川成都";
+        }
+
+        private void LoadRestaurant()
+        {
+            XmlDataService ds = new XmlDataService();
+            var dishes = ds.GetAllDishes();
+            this.DishMenu = new List<DishMenuItemViewModel>();
+            foreach(var dish in dishes)
+            {
+                DishMenuItemViewModel item = new DishMenuItemViewModel();
+                item.Dish = dish;
+                this.DishMenu.Add(item);
+            }
+        }
+
+        private void PlaceOrderCommandExecute()
+        {
+            var selectedDishes = this.DishMenu.Where(i => i.IsSelected == true).Select(i => i.Dish.Name).ToList();
+            IOrderService orderService = new MockOrderService();
+            MessageBox.Show("订餐成功");
+        }
+
+        private void SelectMenuItemExecute()
+        {
+            this.Count = this.DishMenu.Count(i => i.IsSelected == true);
+        }
+    }
+}
